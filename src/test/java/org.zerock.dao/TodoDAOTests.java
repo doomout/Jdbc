0e2d594dde8f81b08ec60a6c290fd61dc322f40d
김현구 <doomout1@gmail.com>
2023-03-18 16:33:51 +0900

    TodoDAO 목록 기능 구현

--- src/test/java/org.zerock.dao/TodoDAOTests.java
+++ src/test/java/org.zerock.dao/TodoDAOTests.java
@@ -6,6 +6,7 @@ import org.zerock.jdbcex.dao.TodoDAO;
 import org.zerock.jdbcex.domain.TodoVO;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TodoDAOTests {
     private TodoDAO todoDAO;
@@ -27,5 +28,10 @@ public class TodoDAOTests {
                 .build();
 
         todoDAO.insert(todoVO);
+    }
+    @Test
+    public void testList() throws Exception {
+        List<TodoVO> list = todoDAO.selectAll();
+        list.forEach(vo -> System.out.println(vo));
     }
 }
